ab017c24e09c1ed269a2a6c282cd9fd9f787585b
Opher <opher.donchin@gmail.com>
2019-07-03 08:35:06 +0300

    Fixing WhiteNoise accorind to their website

--- liyapilates/wsgi.py
+++ liyapilates/wsgi.py
@@ -11,10 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import DjangoWhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'liyapilates.settings')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bootcamp.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
