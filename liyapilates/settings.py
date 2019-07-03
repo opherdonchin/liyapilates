cabeb5fcd1bbc9fad5cc08f01195ee094cec8f4a
Opher <opher.donchin@gmail.com>
2019-07-03 07:44:33 +0300

    Added WhiteNoise

--- liyapilates/settings.py
+++ liyapilates/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -135,6 +136,6 @@ STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static'))
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
